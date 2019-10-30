e6847712ee802df745358e1e648886a65b646a42
Elliot Han <elliotjhan@gmail.com>
2019-10-30 14:49:00 -0700

    began scoring logic, created a class to handle all the scoring mechanics

--- main.js
+++ main.js
@@ -1,22 +1,51 @@
 $(document).ready(initializeApp)
 
 function initializeApp() {
-
+    let initializeScoring = new Scoring;
 }
 
 class Scoring {
     constructor(){
         this.score = null;
-        this.pins = 10;
-        this.turn = 1;
-        
+        this.currentPins = 10; // initialize number of pins every frame at 10
+        this.frame = 1; // tracks the current frame from 1 - 10
+        this.firstOrSecondBowl = 1; // tracks if it's the first or second bowl of the current frame
+        this.spare = false; 
+        this.strike = false;
     }
 
     generatePinsKnockedDown() {
-        let bowl = Math.floor(Math.random() * this.pins + 1);
-        this.pins -= bowl;
+        let numberOfPinsHit = Math.floor(Math.random() * (this.pins + 1));
+        this.currentPins -= numberOfPinsHit;
+    }
+
+    resetFrame() {
+        this.pins = 10;
+        this.frame++;
+    }
+
+    simulateFirstBowl() {
+        this.generatePinsKnockedDown();
+        if(this.pins !== 0) { // checks if the first bowl was a strike
+            this.firstOrSecondBowl++; // if it wasn't a strike move on to the second bowl
+        }
+    }
+
+    simulateSecondBowl() {
+        this.generatePinsKnockedDown();
+        this.firstOrSecondBowl--; // after the second bowl is completed, decrement this.firstorsecondbowl 
+        this.resetFrame(); // will always move to the next frame after the second bowl
+    }
+
+    calculateCurrentScore() {
+        if(this.firstOrSecondBowl === 1) {
+            this.simulateFirstBowl();
+        } else {
+            this.simulateSecondBowl();
+        }
     }
 
 
-
 }
+
+
